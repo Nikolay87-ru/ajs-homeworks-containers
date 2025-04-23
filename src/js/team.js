05d6dc8eb8bf1281c9548fa27c8d03e1260f2db6
Neto test <nikolajeliseev834@gmail.com>
2025-04-23 18:55:31 +0300

    написаны функции для модуля team

---
+++ src/js/team.js
@@ -0,0 +1,22 @@
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(hero) {
+    if (this.members.has(hero)) {
+      throw new Error("Этот герой уже присутствует в команде");
+    }
+    this.members.set(hero);
+  }
+
+  addAll(...heroes) {
+    heroes.forEach((hero) => this.members.set(hero));
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+}
+
+export default Team;
