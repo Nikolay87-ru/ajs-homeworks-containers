b389f9da2ebfaf4b8de2edc0b58ab0491ffcefe2
Neto test <nikolajeliseev834@gmail.com>
2025-04-23 21:40:53 +0300

    добавил остальные тесты

--- src/js/__tests__/team.test.js
+++ src/js/__tests__/team.test.js
@@ -9,3 +9,32 @@ test("should add hero to team", () => {
   expect(team.toArray()).toEqual([hero]);
 });
 
+test("should throw error when add same hero to team", () => {
+  const team = new Team();
+  const hero = { name: "Варвар", health: 90 };
+
+  team.add(hero);
+
+  expect(() => team.add(hero)).toThrow("Этот герой уже присутствует в команде");
+});
+
+test("should add some heroes in team without duplication", () => {
+  const team = new Team();
+  const hero1 = { name: "Варвар", health: 90 };
+  const hero2 = { name: "Волшебница", health: 50 };
+
+  team.addAll(hero1, hero2, hero1);
+
+  expect(team.toArray()).toEqual([hero1, hero2]);
+});
+
+test("should convert object Set to array", () => {
+  const team = new Team();
+  const hero = { name: "Варвар", health: 90 };
+
+  team.add(hero);
+  const array = team.toArray();
+
+  expect(Array.isArray(array)).toBe(true);
+  expect(array).toContain(hero);
+});
