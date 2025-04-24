10eead38a8c40654dbfa801f62e847b6fd74ea65
Neto test <nikolajeliseev834@gmail.com>
2025-04-24 14:29:03 +0300

    добавлен класс ErrorRepository

---
+++ src/js/ErrorRepository.js
@@ -0,0 +1,13 @@
+export default class ErrorRepository {
+  constructor() {
+    this.errors = new Map();
+  }
+
+  add(code, description) {
+    this.errors.set(code, description);
+  }
+
+  translate(code) {
+    return this.errors.has(code) ? this.errors.get(code) : "Unknown error";
+  }
+}
